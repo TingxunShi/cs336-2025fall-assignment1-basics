619a0c0a996d95068d9c30925a1878f0989ee899
txshi <tingxun.shi@gmail.com>
2025-09-22 22:58:30 +0800

    Better checkpoint funcs, referred official implementation for optimization on cuda device

--- cs336_basics/data.py
+++ cs336_basics/data.py
@@ -11,7 +11,15 @@ def get_batch(x: ndarray, batch_size: int, context_length: int, device: torch.de
     input_indices = start_indices + sub_indices
     target_indices = input_indices + 1
 
-    inputs = torch.tensor(x[input_indices], dtype=torch.long, device=device)
-    targets = torch.tensor(x[target_indices], dtype=torch.long, device=device)
+    inputs = torch.from_numpy(x[input_indices].astype(np.int64)).to(device)
+    targets = torch.from_numpy(x[target_indices].astype(np.int64)).to(device)
+
+    device_str = str(device)
+    if 'cuda' in device_str:
+        inputs = inputs.pin_memory().to(device, non_blocking=True)
+        targets = targets.pin_memory().to(device, non_blocking=True)
+    else:
+        inputs = inputs.to(device)
+        targets = targets.to(device)
 
     return inputs, targets
